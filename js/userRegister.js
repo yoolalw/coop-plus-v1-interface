557dcb3c4343a835f0d4f34395353286da218540
yoolalw <larakemilyprofissional@gmail.com>
2026-08-21 09:57:02 -0300

    feat: Adicionando container de opção de perfil de usuario

---
+++ js/userRegister.js
@@ -0,0 +1,10 @@
+const choice1 = document.getElementById('choice_1')
+const choice2 = document.getElementById('choice_2')
+const containerChoices = document.getElementById('containerChoices')
+
+choice1.addEventListener('click', async (e) => {
+    e.preventDefault()
+        
+})
+
+    
