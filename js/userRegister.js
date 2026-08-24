4d7084dff70c8260b1fe25a8e0f2aebeeda9e7e7
yoolalw <larakemilyprofissional@gmail.com>
2026-08-24 15:37:18 -0300

    feat: Criando javascript de registro de usuarios

--- js/userRegister.js
+++ js/userRegister.js
@@ -1,16 +1,25 @@
+const containerEmployee = document.getElementById('containerEmployeeRegister')
 const containerChoice = document.getElementById('containerChoice')
-const choiceEmployee = document.getElementById('choiceEmployee')
-const choiceClient = document.getElementById('choiceClient')
+const containerClient = document.getElementById('containerClientRegister')
 
-containerChoice.style.display = 'none'
+containerEmployee.style.visibility = "hidden"
+containerClient.style.visibility = "hidden"
 
 choiceEmployee.addEventListener('click', (e) => {
     e.preventDefault()
+    choiceRegisterNewEmployee()
+})
 
-    containerChoice.style.display = 'none'
+choiceClient.addEventListener('click', (e) => {
+    e.preventDefault()
+    choiceRegisterNewClient()
+})
+
+function choiceRegisterNewClient() {
+    containerChoice.style.display = "none"
+    containerEmployee.style.visibility = "visible"
 
     const form = document.getElementById('formEmployee')
-    const formData = new FormData(form)
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault()
@@ -27,18 +36,68 @@ choiceEmployee.addEventListener('click', (e) => {
             senha: formData.get('empSenha'),
             avaliacao: 0
         }
+
+        try {
+            const response = await fetch('http://localhost:8080/auth/newEmployee', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            if (!response.ok) {
+                console.error("Ocorreu um erro ao tentar se cadastrar, tente novamente.", response.status)
+                return
+            }
+
+        } catch (error) {
+            console.log("Ocorreu um erro durante a conexão do servidor! ", error)
+            throw error
+        }
+
     })
-
-    try{
-
-        fetch('http://localhost:8080/newEmployee', {
-            method: 'POST',
-            body: JSON.stringify(object)
-        })
-
-    }catch(e){
-        console.error('Ocorreu um erro durante a tentativa de requisição! \n Erro obtido: ' + e)
-    }
+}
 
 
-})
+function choiceRegisterNewEmployee() {
+    containerChoice.style.display = "none"
+    containerEmployee.style.visibility = "visible"
+
+    const form = document.getElementById('formEmployee')
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+
+        const formData = new FormData(form)
+
+        const object = {
+            nomeCompleto: formData.get('empNomeCompleto'),
+            email: formData.get('empEmail'),
+            telefone: formData.get('empTelefone'),
+            cnpj: formData.get('empCnpj'),
+            tipoServico: formData.get('empTipoServico'),
+            nomeEmpresa: formData.get('empNomeEmpresa'),
+            senha: formData.get('empSenha'),
+            avaliacao: 0
+        }
+
+        try {
+            const response = await fetch('http://localhost:8080/auth/newEmployee', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            if (!response.ok) {
+                console.error("Ocorreu um erro ao tentar se cadastrar, tente novamente.", response.status)
+                return
+            }
+
+        } catch (error) {
+            console.log("Ocorreu um erro durante a conexão do servidor! ", error)
+            throw error
+        }
+
+    })
+}
